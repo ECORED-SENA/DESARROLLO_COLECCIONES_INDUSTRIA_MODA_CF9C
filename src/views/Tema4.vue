e3c7dce10cc17813c614cadf9e3c37bc4aff6753
Leo <83103793+Leoska89@users.noreply.github.com>
2021-12-05 16:15:15 -0500

    avance 3

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -190,10 +190,57 @@
               p.mb-0 El peso de las telas depende totalmente del tipo de tejido en el que se hace, este está determinado y dado comercialmente en onzas/-Yardas2 para referirse al peso de las telas tipo jeans wear. Se consideran telas livianas tipo dénim de 9 onzas/-Yardas2, y pesadas a partir de 9.5 onzas/-Yardas2. Los pesos de las telas dénim pueden cambiar y tienen una tolerancia de +/- 5%, según la norma NTC 5656. En las telas stretch y con altos encogimientos, tienden a ganar peso después de los procesos de lavandería. Por ello, el peso crudo y el peso lavado se informan en la ficha técnica. Además, esto se debe tener muy en cuenta al trazar el número de patrones. (Tèxtil Balsareny, 2019)
                 
 
+    p.mb-4 La marcada o tizada se divide según el tipo de tejido. 
+
+    .row.justify-content-center.mb-2
+      .col-lg-10
+        .p-4.py-5(style="background-color: #F2D6E3")
+          ul.lista-ul.mb-0
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0
+                b Tizado en tela punto: 
+                b-none la tela a emplear es jersey 30/1, 24/1, inter look, franela. El tipo de acabado es de color entero, tela listada. Cuando se tiza en tela de punto es recomendable dejar una separación entre patrón y patrón de 3mm, para prevenir el encogimiento de la prenda después de cortada.
+
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        .p-4.py-5(style="background-color: #C2FCFA")
+          ul.lista-ul.mb-0
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0
+                b Tizado o trazo en tela plana: 
+                b-none el tipo de tela puede ser dénim, popelina, drill, bramante, corduroy, seda. El tipo de acabado es de color entero, listada a cuadros, entre otros. El tizado en tela plana es más sencillo que en telas listadas o a cuadros, en las cuales no se pueden hacer variaciones en la orientación de la línea de aplomo y se debe respetar rigurosamente el orden de las líneas del trazado.
 
 
+    p.mb-5 El procedimiento del tizado se inicia cuando el responsable tiene la solicitud de confección definida, las especificaciones de la prenda, todo el juego de los patrones y las muestras de las prendas respectivas. Para la programación del tizado se recomienda:
 
-
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        p.mb-5.text-center
+          b(style="color: #BF3976;") PROGRAMACIÓN TIZADO
+        
+        PasosA.color-secundario.mb-5(tipo="l")
+          .row
+            .col-auto
+              p.mb-0 Medir el ancho de la tela.
+                
+          .row
+            .col-auto
+              p.mb-0 Iniciar la tarea de derecha a izquierda, comenzando por las partes más grandes del paquete de patrones, las que determinarán el largo del tizado.
+                
+          .row
+            .col-auto
+              p.mb-0 Que el ancho del papel sea menor en 1 cm al ancho de la tela utilizable.
+                
+          .row
+            .col-auto
+              p.mb-0 Que todos los patrones tengan el aplomo que determinará la caída de la prenda y su lugar en el trazo.
+                
+          .row
+            .col-auto
+              p.mb-0 Que los lados críticos para cortar estén hacia el lado donde se ubica el trazador y posteriormente el cortador. Un ejemplo de lados críticos a la hora de trazar y cortar una camisa es: el escote delantero, la almilla, el cuello, el pie de cuello.
+                
 
 
 
@@ -203,19 +250,265 @@
       .h2 4.1 Programación de corta
     
     
+    .row.justify-content-center.mb-5
+      .bloque-texto-a.color-primario.p-4.p-md-5.mb-5 
+        .row.m-0.align-items-center.justify-content-between
+          .col-lg-3.mb-4.mb-lg-0
+            figure.mb-0
+              img(src='@/assets/curso/tema4/img14.svg', alt='', style='width: 220px;').m-auto
+          .col-lg-9
+            .bloque-texto-a__texto.p-5
+              p.mb-0 Dibujo a mano del perfil de los patrones sobre una superficie plana, ya sea sobre papel o tela. Si se ha de repetir la marcada y esta se hace sobre papel, se pueden hacer copias colocando varios papeles superpuestos e intercalando papel carbón entre ellos. Una vez efectuado el corte, la marcada queda destruida.
+                
+
+
+    p.mb-5 Ya no se emplea en empresas, por tener altos ciclos de producción y ser muy costoso, es engorroso manejar pieza por pieza en cartón todas las tallas de toda la prenda, sobreponerlas en el papel y marcar los contornos intentando un aprovechamiento eficiente de la tela cuando los volúmenes son muy grandes.  
+
+
+    .row.justify-content-center.align-itmes-center.mb-5
+      .col-lg-5.mb-4.mb-lg-0
+        .cajon.color-primario.p-4.ps-lg-5
+          p.mb-4 Los materiales para realizar un trazo manual son: 
+          ul.lista-ul.mb-0
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Lápiz de buena mina.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Lápiz blanco.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Tiza si es para trazar muestras.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Reglas y escuadras.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Tener las medidas del rollo de la tela.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Papel kraft para realizar el trazo.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Juego de patrones.
+          
+      .col-8.col-lg-7
+        figure.mb-0
+          img(src='@/assets/curso/tema4/img15.png', alt='imagen ambiental', style='width: 700px;').m-auto
+
+    p.mb-4
+      b La colocación de los patrones en el tejido no se hace de forma aleatoria. La dirección del hilo afecta la apariencia, construcción y caída de la prenda.
+
+    p.mb-4 No es raro que una camiseta se gire en el bajo o que el cuello se estire en cada puesta. Todos estos inconvenientes tienen su origen en la mala colocación del patrón encima del tejido, no estando alineado con el hilo adecuado.
+    p.mb-5 Cada dirección de hilo tiene unas características apropiadas según el efecto o tipo de pieza que se quiera cortar. La dirección del hilo siempre viene marcada en el patrón con una flecha de doble punta, que hay que hacer coincidir con el recto hilo de la tela. Los tipos de trazo que se tienen en cuenta son:
+
+    .row.justify-content-center.mb-5
+      .col-11.mb-5
+        .tarjeta.tarjeta--azuverde.p-4.py-lg-5(style="border-radius: 0;")
+          SlyderA
+            .row.align-items-top.justify-content-center
+              .col-11.col-md-5.mb-4.mb-md-0
+                h4.mb-4 Recto hilo
+                p Es la dirección de los hilos verticales del tejido (urdimbre) y es paralelo a los orillos. Una prenda se traza al recto hilo cuando el centro delantero o centro espalda es paralelo al recto hilo. La urdimbre (hilos verticales) aporta estabilidad a la prenda, mientras que la trama (hilos horizontales) añade un margen de elasticidad al contorno del cuerpo. 
+                p.mb-0 La mayor parte de prendas se trazan al recto hilo, incluyendo pantalones, faldas y camisas. (Prieto, 2015)
+                  
+              .col-10.col-md-5
+                figure
+                  img(src='@/assets/curso/tema4/img16.png', alt='imagen ambiental', style='width: 400px;').m-auto
+                    
+            .row.align-items-top.justify-content-center
+              .col-11.col-md-5.mb-4.mb-md-0
+                h4.mb-4 Contrahílo
+                p.mb-0 Es aquella prenda que tiene la dirección del hilo a 90º respecto del centro del delantero o de la espalda. En ocasiones, algunas piezas de la prenda se trazan a contrahílo para ayudar a estrechar determinadas partes. Los puños, el cuello, el canesú y la almilla de la camisa se trazan a contrahílo para controlar la flexibilidad de la manga, el escote y la espalda trazados al recto hilo. Otras veces, se traza a contrahílo para crear un impacto visual con algún estampado. Al contrahílo es trazar también sobre la trama del tejido. (Prieto, 2015)
+                  
+              .col-10.col-md-5
+                figure
+                  img(src='@/assets/curso/tema4/img17.png', alt='imagen ambiental', style='width: 420px;').m-auto
+                    
+            .row.align-items-top.justify-content-center
+              .col-11.col-md-5.mb-4.mb-md-0
+                h4.mb-4 Bies o al sesgo
+                p.mb-0 Se traza cuando la dirección del hilo se encuentra a 45º del centro delantero o espalda. Las prendas tienen una flexibilidad extra, se ciñen más al cuerpo y caen relajadamente. (Prieto, 2015)
+                  
+              .col-10.col-md-5
+                figure
+                  img(src='@/assets/curso/tema4/img18.png', alt='imagen ambiental', style='width: 400px;').m-auto
+                    
+
+    h4 Sistema perforado
+    p.mb-5 Se efectúa el trazo (marcado) sobre papel; una vez dibujado, se procede a perforar el contorno de cada una de las piezas o figuras con un pin o punzón. Para reproducir esta marcada sobre el tejido, basta colocarlo sobre el mismo y espolvorearlo con polvos tipo negro humo o talco, pasando estos tejidos y quedando adheridos a él; la marcada tiene una larga duración. Este se realiza también para marcar zonas específicas en confección, como dibujo de bolsillos, curvas y aquellas operaciones complejas que sin una guía no se lograrían. (Sena, 2012)
+
+
+    h4 Sistema por pulverización
+    p.mb-5 Los patrones deben estar completos (ejemplo: los patrones marcados 2 x talla y marcados izquierdo y derecho). Se colocan estos sobre el tejido o el papel como armando un rompecabezas. Terminado el trazo, se procede a espolvorear (polvos tipo negro humo o talco) en los espacios vacíos, quedando adheridos al material   y marcada la silueta de los moldes. Se completa la marcada colocando un adhesivo (cinta pegante) en el cual se coloca la información deseada (talla, lote, etc.)
+
+    h4 Sistema fotográfico
+    p.mb-5 Se extiende el papel fotográfico sensibilizado sobre la mesa, se depositan los patrones sobre él, siguiendo la posición del estudio previo. Se pasa, seguidamente, un carro que transporta potentes focos de luz ultravioleta que actúan sobre las partes del papel que está al descubierto, luego, se fija esta imagen escalando el papel en atmósfera amoniacal. El perfil de los patrones queda marcado con extrema nitidez. (Sena, 2012).
+
     
     #t_4_2.titulo-segundo.color-acento-contenido
-      .h2 4.2 Liquidación de corte
+      .h2 4.2 Proceso de miniaturización
     
     
+
+    p.mb-5 En este proceso, se reducen los patrones de escala real a diferentes escalas menores de 1/5, 1/3, 1/8, con el fin de realizar trazos previos para obtener promedios de materia prima sin gastar papel, para luego hacer el trazo real, ahorrando costos.
+
+    .row.align-items-center.mb-5
+      .col-lg-5.mb-4.mb-lg-0
+        .cajon.color-primario.p-4.ps-lg-5
+          p.mb-4
+            b La miniaturización permite: 
+            
+          ul.lista-ul.mb-0
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Realizar estudios previos de marcadas.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Conocer consumos o promedios.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Obtener el largo del trazo.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Programar el trazador según promedios.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Dimensionar la longitud del rollo.
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Reducir costos en material para trazar y en tela.
+                
+      .col-lg-6
+        .row.justify-content-center
+          .col-10.col-lg-8
+            figure
+              img(src='@/assets/curso/tema4/img19.svg', alt='imagen ambiental', style='width: 219px;').m-auto
+                    
+          
+
+
     #t_4_3.titulo-segundo.color-acento-contenido
       .h2 4.3 Trazo sistematizado
     
     
-    
+    p.mb-4 El diseño asistido por computador – CAD – es el uso de herramientas tecnológicas o software especializado para ayudar a desarrollar tareas que antes se hacían de forma manual. El otro sistema de trazo es de manera sistematizada por medio de un software específico, el cual viene, en su mayoría, dividido en 2, el primer sistema es el de patrones y el segundo, el de “marcada o tizada”. 
+    p.mb-5 Los patrones sistematizados se obtienen mediante la realización de ellos desde cero o por medio de la digitalización. Digitizer es un software que permite digitalizar moldes físicos, es decir, pasarlos al sistema computarizado por medio de puntos que toman fielmente los contornos del molde, los cuales podrán ser modificados y escalados posteriormente de la misma manera que se trabaja con los patrones creados directamente en el programa. La digitalización puede hacerse usando una mesa digitalizadora, que traslada los contornos de un molde al sistema, o a través de la fotografía digital. El software facilita la disposición de las piezas o moldes en la mesa de corte para minimizar el desperdicio de tela y así ahorrar en los costos de material y tiempo de trabajo. El trazo puede hacerse de forma automática o semiautomática y solamente es necesario indicar las medidas de la tela para que el software opere por su cuenta automáticamente. Es mucho más eficaz y genera mayor productividad que el sistema manual, ya que este se logra por medio de algoritmos. (Sena, 2012)
+
+
+    .row.mb-5
+      .col-lg-5
+        .cajon.color-primario.p-4.ps-lg-5
+          p.mb-4
+            b El sistema pide datos exactos para realizar el trazo, tales como los del siguiente ejemplo:: 
+            
+          ul.lista-ul.mb-0
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Referencia: 0034
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Tipo de prenda: Blusa camisera femenina
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Talla: M
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Tela: Popelina 
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Tejido: Plano  
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Ancho de tela: 158 cm
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Orillos: 2.5 cm c/lado
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Tolerancia: 2 cm
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Ancho útil: 153 cm
+                
+            li
+              i.fas.fa-check(style='color: #BF3976;')
+              p.mb-0 Número de marcaciones: 1
+                
+
+
+    p.mb-5 La siguiente figura representa este tipo de trazo:
+
+    .titulo-sexto.color-acento-contenido.mb-4
+      span #[strong Figura 4] Trazo sistematizado
+
+    .row.justify-content-center.mb-5
+      .col-lg-8
+        figure
+          img(src='@/assets/curso/tema4/img20.png', alt='imagen ambiental', style='').m-auto
+
+
+    p.mb-5 Las marcaciones hacen referencia al número de veces en que una prenda está trazada sobre el área de trabajo. La talla puede repetirse, por lo tanto, se cuenta como una marcación. Una marcación corresponde a una prenda marcada, dos marcaciones a dos prendas marcadas y así sucesivamente. Se manejan dos tipos de trazo de acuerdo con el vello en ambos sentidos (bidireccional) o en un solo sentido (direccional):
+
+    .row.justify-content-center.mb-5
+      .col-lg-6.mb-4.mb-lg-0
+        .cajon-b.color-secundario.p-3.mb-0
+          p
+            b Bidireccional
+          p.mb-0 Se elabora el trazo o marcado colocando en dos sentidos de dirección todos los patrones o piezas de todas las tallas que se van a cortar (teniendo en cuenta la línea de aplome o hilo de urdimbre, el patrón se mueve de forma horizontal hacia la izquierda o la derecha). El material textil que se utilice no debe tener restricción con respecto a sus características (brillo u otra característica), por lo tanto, se empleará menos cantidad de tela. Según el producto, la eficacia del marcador se aproximará a un promedio del 81-85 %. Esto quiere decir el poder ubicar cada pieza que compone la prenda en el ancho y largo de la tela para lograr un consumo óptimo de la tela, sin desperdicio de material, y lo ideal es que llegue a más del 80 %.
+      .col-lg-6
+        figure
+          img(src='@/assets/curso/tema4/img21.png', alt='imagen ambiental', style='').m-auto
+
+
+    .row.justify-content-center.mb-5
+      .col-lg-6
+        figure
+          img(src='@/assets/curso/tema4/img22.png', alt='imagen ambiental', style='').m-auto
+      .col-lg-6.mb-4.mb-lg-0
+        .cajon-b.color-acento-contenido.p-3.mb-0
+          p
+            b Direccional
+          p.mb-0 Se elabora el trazo o marcado colocando en un sentido de dirección todos los patrones o piezas de todas las tallas que se van a cortar. Este procedimiento de marcado tendrá por resultado un producto con la más elevada calidad, pero con mayor consumo de tela. Este tipo de trazo se usa para materiales que cuentan con restricción con respecto a sus características, en cuanto a brillo, cambio de color al tacto, estampados, dirección de pelos, entre otros. Según el producto, la eficacia del marcador tendrá una utilización promedio aproximado al 77- 79 %.
+
+
     #t_4_4.titulo-segundo.color-acento-contenido
       .h2 4.4 Trazo en telas con rayas, cuadros, estampados y tejido con pelo
-    
+
+
+    p.mb-5 Una de las cosas a tener en cuenta a la hora de comprar tela con rayas y/o cuadros es la regularidad o irregularidad de su diseño. Después de identificar la regularidad de la tela, existen diferentes precauciones que se deben tener a la hora de hacer los trazos:
+
+    .row.mb-5
+      .col-lg-8.mb-5
+        LineaTiempoD.color-secundario
+          .row(numero="1" titulo="Identificar diseño regular o irregular")
+            .col-md-12.mb-4.mb-md-0
+              p.mb-0 Azul claro, blanco, rosado, amarillo y beige.
+                
+
+
+
+
+
     
     #t_4_5.titulo-segundo.color-acento-contenido
       .h2 4.5 Espigas en el trazo
@@ -238,7 +531,7 @@
       .h2 4.8 Ploteo de trazos
 
 
-    tab-custom
+    //tab-custom
       .row.justify-content-center(titulo="Herramientas de medición y construcción")
         .col-lg-10.mt-5.mb-5
           .cajon.color-primario.p-5.mb-4
