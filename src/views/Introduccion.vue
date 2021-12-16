8c4c23705da6498ec19044c787713d643c96b61d
Leo <83103793+Leoska89@users.noreply.github.com>
2021-12-16 14:29:28 -0500

    video introduccion y material.zip

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -17,7 +17,7 @@
       .col-lg-10
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            <iframe width="950" height="534" src="https://www.youtube.com/embed/PH0IIe2IQWw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       
       
     .cajon.color-primario.p-5
